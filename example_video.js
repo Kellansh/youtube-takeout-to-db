9332e3cc935b37434317a7870465266b6df8de45
Kellansh <118218050+Kellansh@users.noreply.github.com>
2024-12-21 13:32:15 -0500

    Add files via upload

---
+++ example_video.js
@@ -0,0 +1,21 @@
+info = {
+    "version": "1.0",
+    "thumbnail_height": 360,
+    "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    "provider_name": "YouTube",
+    "thumbnail_width": 480,
+    "width": 200,
+    "html": "<iframe width=\"200\" height=\"113\" src=\"https://www.youtube.com/embed/dQw4w9WgXcQ?feature=oembed\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen title=\"Rick Astley - Never Gonna Give You Up (Official Music Video)\"></iframe>",
+    "height": 113,
+    "provider_url": "https://www.youtube.com/",
+    "type": "video",
+    "title": "Rick Astley - Never Gonna Give You Up (Official Music Video)",
+    "thumbnail_url": "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg",
+    "author_url": "https://www.youtube.com/@RickAstleyYT",
+    "author_name": "Rick Astley"
+}
+
+// what I need:
+// title
+// author name
+// image downloaded from thumbnail url
---
+++ get_details.py
@@ -0,0 +1,41 @@
+""" 
+Gist:
+
+1. Take in playlist
+
+2. Add columns: title, author name, thumbnail name, freeform notes
+
+3. For each row/video ID:
+
+3a. Grab info from noembed
+
+3b. Download image from thumbnail URL
+
+3c. Append  title and author name
+
+4. Format playlist to import if needed
+
+"""
+
+import pandas as pd
+import numpy as np
+import requests
+
+df = pd.read_csv("example_playlist.csv", header=0)
+
+def get_noembed_info(row, attribute):
+    res = requests.get('https://noembed.com/embed?url=https://www.youtube.com/watch?v='+row['Video ID']).json()
+
+    if 'error' in res:
+        print('Error for row',row.index,'with video ID',row['Video ID'])
+        print('Output from noembed:',res['error'])
+        return res['error'], res['error']
+    else:
+        return res[attribute]
+
+df['Title'] = df.apply(get_noembed_info, attribute=('title'), axis=1)
+df['Uploader'] = df.apply(get_noembed_info, attribute=('author_name'), axis=1)
+
+# next step: ability to optionally download each thumbnail (with video ID as image name)
+
+print(df.head())
