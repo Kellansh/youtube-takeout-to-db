555f376acdbb6efd47325ffbd59376d1e2b470e8
Kellan Heth <kellansh@gmail.com>
2026-06-20 13:38:06 -0400

    Continue setting up Django

--- youtubetakeout/settings.py
+++ youtubetakeout/settings.py
@@ -74,9 +74,12 @@ WSGI_APPLICATION = 'youtubetakeout.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "my_service",
+            "passfile": ".my_pgpass",
+        },
     }
 }
 
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
